680736ba7cbd0fd4a8d461da146b07dad5948890
anami823 <anami823@gmail.com>
2019-07-26 16:22:36 -0700

    Create board array

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@
 /*----- cached element references -----*/ 
 /*----- event listeners -----*/ 
 /*----- functions -----*/
+
+/* 
+When I click a box mark it with an x
+*/
+
+let board;
+
+const initialize = () => {
+    console.log('Initialized')
+    console.log(board)
+}
+
+ board = Array(3).fill(Array(3).fill(null))
+
+ /*
+const iterateBoard = () => {
+
+} */
+
+initialize()
