ba07da688c324ad39e02b7ede18d68bd53370971
anami823 <anami823@gmail.com>
2019-07-26 19:20:24 -0700

    Create play function to mark cell on click

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ Game Story
 */
 
 
-/*----- constants -----*/ 
+/*----- constants -----*/
+let board, turn;
 /*----- app's state (variables) -----*/ 
 /*----- cached element references -----*/ 
 /*----- event listeners -----*/ 
@@ -13,13 +14,8 @@ Game Story
 
 /*----- functions -----*/
 
-/* 
-When I click a box mark it with an x
-*/
-
-let board;
-
 const initialize = () => {
+    turn = true
     console.log('Initialized')
     board = Array(3).fill(Array(3).fill(null))
     console.log(board)
@@ -27,19 +23,30 @@ const initialize = () => {
 
 initialize()
 
-//Assign id's to each cell 
+
+// When I click a box mark it with an x
+const play = (evt) => {
+    console.log('Click!: ' + evt.target.getAttribute('data-position'))
+    if (turn) {
+        console.log(evt.target)
+        evt.target.textContent = 'X'
+    } else {
+        evt.target.textContent = 'O'
+    }
+    turn = !turn
+}
+
+//Create cell elements
 const iterateBoard = () => {
     board.forEach(function (rowArr, rowIdx){
         rowArr.forEach(function (cellValue, colIdx){
-            //console.log(`r${rowIdx}c${colIdx}`)
-            //console.log(cellValue)
             const boardEl = document.querySelector('#board')
-            console.log('boardEl: ' + boardEl)
             const cellEl = document.createElement('div')
-            cellEl.setAttribute('data-position', `r${rowIdx}c${colIdx}`)
-            console.log('position: ' + cellEl.getAttribute('data-position'))
+            //cellEl.setAttribute('data-position', `r${rowIdx}c${colIdx}`)
+            //console.log('position: ' + cellEl.getAttribute('data-position'))
             cellEl.classList.add('cell')
-            boardEl.appendChild(cellEl)          
+            boardEl.appendChild(cellEl)
+            cellEl.addEventListener("click", play)          
         })
     })
 }
